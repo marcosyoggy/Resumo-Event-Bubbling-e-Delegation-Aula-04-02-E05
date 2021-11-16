bb61b371857778a9fecd36ada1e8b284e566118f
marcosyoggy <44072541+marcosyoggy@users.noreply.github.com>
2021-11-16 20:20:23 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,37 +1,36 @@
+
+// https://github.com/marcosyoggy/Resumo-Event-Bubbling-e-Delegation-Aula-04-02-E05 
+
+// https://resumo-event-bubbling-e-delegation-aula-04-02-e05.netlify.app
+
+
+
 //---------------BLOCK CONST-------------------------------------
 
 const parentBody = document.querySelector(".parentBody")
-const blockAcessButtons = document.querySelector(".blockAcessButtons")
 const divOfExitButtons = document.querySelector(".divOfExitButtons")
 
-//---------------BLOCK FUNCTIONS-------------------------------------
-
-const hiddenBlocks = event => {
+const toggleBlockDisplay = event => {
     const clickedElement = event.target
     const datasetExitButtonValue = clickedElement.dataset.exitbutton
+    const datasetAcessButtonValue = clickedElement.dataset.acessbutton
+    const arrayExitButtons = Array.from(divOfExitButtons.children)
     const parentRef = document.querySelector(`[data-blockdiv="${datasetExitButtonValue}"]`)
 
     if (datasetExitButtonValue) {
         parentRef.style.display = 'none'
         return
     }
-}
-
-const showBlocks = event => {
-    const clickedElement = event.target
-    const datasetAcessButtonValue = clickedElement.dataset.acessbutton
-    const arrayExitButtons = Array.from(divOfExitButtons.children)
 
     if (datasetAcessButtonValue) {
         arrayExitButtons.forEach(item => {
             const datasetLiBlockValue = item.dataset.blockdiv
             const datasetAcessLastString = datasetAcessButtonValue.slice(-1)
             const liDatasetLastString = datasetLiBlockValue.slice(-1)
-            const arrayLiClasslist = Array.from(item.classList)
 
             if (item.style.display.includes("none")) {
                 if (datasetAcessLastString === liDatasetLastString) {
-                    item.style.display = 'block'                
+                    item.style.display = 'block'
                     return
                 }
             }
@@ -39,19 +38,8 @@ const showBlocks = event => {
     }
 }
 
-//---------------BLOCK ACTIONS---------------------------------
 
-parentBody.addEventListener("click", hiddenBlocks)
-
-blockAcessButtons.addEventListener("click", showBlocks)
-
-
-
-
-
-
-
-
+parentBody.addEventListener("click", toggleBlockDisplay)
 
 
 
